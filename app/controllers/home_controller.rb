a573305542a3c471075fe59ccf44e687e6a91b51
StrangeTim <strang3tim@gmail.com>
2016-12-19 19:07:28 -0800

    incredibly basic app with only 1 route model and view. no database

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,39 @@
+class HomeController < ApplicationController
+
+  def show
+  #  @carousel = []
+  #  carousel_directory = Dir.open "public/carousel"
+
+  #  carousel_directory.each do |image|
+  #    if image.length > 2
+  #      @carousel.push("carousel/"+image)
+  #    end
+  #  end
+  #  @home = Home.find(1)
+  #  @title = @home.title
+  #  @left_header = @home.left_header
+  #  @left_body = @home.left_body
+  #  @right_header = @home.right_header
+  #  @right_body = @home.right_body
+  end
+
+
+  def edit
+  #  @home = Home.find(1)
+  end
+
+  def update
+  #  @home = Home.find(params[:id])
+  #  if @home.update(home_params)
+  #    redirect_to '/'
+  #  else
+  #    render :edit
+  #  end
+  end
+
+  private
+  def home_params
+  #  params.require(:home).permit(:title, :left_header, :left_body, :right_header, :right_body)
+  end
+
+end
